384ca1ba0e12b9365dc67a1d60169030df9baef9
johnnyblack12 <jfebbo5@gmail.com>
2021-04-02 22:23:21 -0400

    Exponent, multiplication, division functions complete

--- js/scientific.js
+++ js/scientific.js
@@ -1,40 +1,107 @@
-let arr = [];
-let decOk = true;
+let expression;
+let decOK = true;
+let opOk = false;
+let negOK = true;
+let leftPar = 0;
+let rightPar = 0;
+let parOK = false;
+
 function calculate(e) {
     if (display.classList.contains('display-scientific')) {
         let val = e.target.id;
+        let last = working.textContent.length - 1;
         // clear button
         if(val == 'clr') {
             working.textContent = '';
             answer.textContent = '';
-            decOk = true;
-            arr = [];
+            decOK = true;
+            opOk = false;
+            negOK = true;
+            paren = false;
+            expOK = false;
+            finish = false;
+            leftPar = 0;
+            rightPar = 0;
+            reduced = 0;
+            expression = null;
         // main I/O
-        } else {
-            let exLen = working.textContent.length;
-            let last = exLen - 1;
-            // accept only negative sign, decimal, or number on first digit
-            if(exLen < 1 && (val == '-' || val == '.' || !isNaN(val))) {
-                working.textContent += val;
-            // block double operator on leading negative
-            } else if (working.textContent == '-' && !isNaN(val)) {
-                working.textContent += val;
-            } else {
-                // accept operators and block doubles
-                if (exLen >= 1 && (val == '+' || val == '-' || val == '*' || val == '/') && working.textContent != '-' &&
-                    (working.textContent[last] != '+' && working.textContent[last] != '-' &&
-                    working.textContent[last] != '*' && working.textContent[last] != '/')) {
-                        decOk = true;
-                        working.textContent += val;
-                // accept numbers
-                } else if (exLen >=1 && !isNaN(val)) {
-                    working.textContent += val;
-                // accept decimal only after number
-                } else if (val == '.' && decOk == true) {
-                    decOk = false;
-                    working.textContent += val;
-                }
+        } else if (val == '=') {
+            if (working.textContent.length > 0 && leftPar == rightPar) {
+                expression = working.textContent;
+                raise(); 
+                times();
+                divided();
+                answer.textContent = expression;
+                console.log(expression);
             }
+        } else {
+            if(!isNaN(val)) {
+                working.textContent += val;
+                opOK = true;
+            } else if (val == '-' && negOK)  {
+                working.textContent += val;
+                negOK = false;
+                decOK = true;
+            } else if ((val == '+' || val == '-' || val == '*' || val == '/' || val == '^') && opOK) {
+                working.textContent += val;
+                opOK = false;
+                negOK = true;
+                decOK = true;
+            } else if (val == '.' && decOK) {
+                working.textContent += val;
+                decOK = false;
+            } else if (val == '(' && leftPar>=rightPar) {
+                working.textContent += val;
+                leftPar++;
+                opOK = false;
+            } else if (val == ')' && rightPar<leftPar && working.textContent[last] != '(' && (working.textContent[last] != '.' &&
+                isNaN(working.textContent[last-1]))) {
+                    working.textContent += val;
+                    rightPar++;
+                    opOK = true;
+            }
+        }
+    }
+}
+
+function raise() {
+    if (/\^/.test(expression) == true) {
+        while(/\^/.test(expression)) {
+            let express1 = /(\d+\.\d+|\d+|\.\d+|\d+\.)\^/.exec(expression)[0];
+            let express2 = /\^(\d+|\d+\.\d+|\.\d+|\d+\.)/.exec(expression)[0];
+            let num1 = Number(express1.slice(0, (express1.length - 1)));
+            let num2 = Number(express2.slice(1));
+            let expressTot = express1.slice(0, (express1.length - 1)) + '^' + express2.slice(1);
+            let result = String(Math.pow(num1, num2));
+            expression = expression.replace(expressTot,result);
+        }
+    }
+}
+
+function times() {
+    if (/\*/.test(expression) == true) {
+        while(/\*/.test(expression)) {
+            let express1 = /(\d+\.\d+|\d+|\.\d+|\d+\.)\*/.exec(expression)[0];
+            let express2 = /\*(\d+|\d+\.\d+|\.\d+|\d+\.)/.exec(expression)[0];
+            let num1 = Number(express1.slice(0, (express1.length - 1)));
+            let num2 = Number(express2.slice(1));
+            let expressTot = express1.slice(0, (express1.length - 1)) + '*' + express2.slice(1);
+            let result = String(num1 * num2);
+            expression = expression.replace(expressTot,result);
+        }
+    }
+}
+
+function divided() {
+    if (/\//.test(expression) == true) {
+        while(/\//.test(expression)) {
+            let express1 = /(\d+\.\d+|\d+|\.\d+|\d+\.)\//.exec(expression)[0];
+            let express2 = /\/(\d+|\d+\.\d+|\.\d+|\d+\.)/.exec(expression)[0];
+            let num1 = Number(express1.slice(0, (express1.length - 1)));
+            let num2 = Number(express2.slice(1));
+            let expressTot = express1.slice(0, (express1.length - 1)) + '/' + express2.slice(1);
+            let result = String(num1 / num2);
+            expression = expression.replace(expressTot,result);
         }
     }
 }
